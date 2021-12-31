04a677ab3436f8b93fc2cfef6417789fb2c334ac
DESKTOP-SSF2K70\User <irfankhoerul12@gmail.com>
2021-12-31 20:01:01 +0700

    FIX : RestoApp

--- makanan.cpp
+++ makanan.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "prototype.h"
-void topping_makanan();
-int pil;
 
-void makananMenu(){
+void getMakanan(){
     // local Variabel
+    int pil;
+
     printf("\t\t|====================== Daftar Makanan ==============|\n");
     printf("\t\t|                                                    |\n");
     printf("\t\t| Nama Makanan                        Harga          |\n");
@@ -22,25 +22,19 @@ void makananMenu(){
         case 1: // Nasi Goreng
         {
             system("cls");
-            printf("Anda memilih : \n");
-            printf("\tMakanan : Nasi Goreng\n");
-            printf("\tHarga   : Rp. 25000\n\n");
-            
-
-            topping_makanan
-            ();
+            printf("\t\t Nasi Goreng\n");
             break;
         }
         case 2: // Ayam Geprek
         {
             system("cls");
-            printf("Ayam Geprek\n");
+            printf("\t\t Ayam Geprek\n");
             break;
         }
         case 3: // Ikan Bakar
         {
             system("cls");
-            printf("Ikan Bakar\n");
+            printf("\t\t Ikan Bakar\n");
             break;
         }
         case 4: // Kembali ke Daftar Menu Pembelian
@@ -57,34 +51,4 @@ void makananMenu(){
         }
     }
 
-}
-
-
-void topping_makanan(){
-    char pilih;
-    printf("\n\t Apakah ingin menggunakan Topping ? (y/t) ");
-    scanf("%s", &pilih);
-
-    if (pilih == 'y' || pilih == 'Y')
-    {
-        printf("\t\t|====================== Daftar Topping ==============|\n");
-        printf("\t\t|                                                    |\n");
-        printf("\t\t| Nama Topping                        Harga          |\n");
-        printf("\t\t| 1. Pedas                            Rp. 1000       |\n");
-        printf("\t\t| 2. Tambah 1/2 porsi                 Rp. 11000      |\n");
-        printf("\t\t| 3. Kembali ke Daftar Menu Pembelian                |\n");
-        printf("\t\t|                                                    |\n");
-        printf("\t\t|====================================================|\n");
-    }else{
-        printf("Apakah anda ingin sudahi pesanan? (y/t) ");
-        scanf("%s", &pilih);
-        if(pilih == 'y' || pilih == 'Y'){
-            printf("Pesanan anda sudah tercatat !");
-        }else if(pilih == 't' || pilih == 'T'){
-            printf("Pesanan anda belum tercatat !");
-        }else{
-            printf("Inputan anda salah !");
-        }
-    }
-   
 }
