5de5e49b4d42d7f27683af0eaa5aca10364e7493
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-01-01 20:45:38 +0700

    FIX : add getch file

--- transaksi.cpp
+++ transaksi.cpp
@@ -2,32 +2,42 @@
 #include "transaksi.h"
 
 void transaksi(){
-    Pembeli dt;
-    FILE *f_pembeli;
+    pelanggan dt_pelanggan;
+    FILE *f_pelanggan;
 
-    if ((f_pembeli = fopen("data_transaksi.dat", "rt")) == NULL)
+    f_pelanggan = fopen("data_transaksi.dat", "rt");
+
+    if (!f_pelanggan)
     {
-        printf("File tidak dapat dibuka\n");
+        printf("ERROR : File tidak dapat dibuka !\n");
         exit(1);
     }
 
-       
+    // create table transaksi
+    printf("|==========================================================================================|\n");
+    printf("|                                      Data Transaksi                                      |\n");
+    printf("|==========================================================================================|\n"); 
+    printf("| ID      Nama      Makanan     Minuman       Harga normal       Diskon       Harga Total  |\n");
+    printf("|==========================================================================================|\n");
 
-    printf("\nISI FILE data_transaksi.dat ADALAH : \n\n");
-    printf("Nama | Pesanan | Harga Toral \n");
-    while (!feof(f_pembeli))
-    {
-        fscanf(f_pembeli, "%s %s %f", &dt.nama, &dt.pesanan, &dt.harga_total);
-        printf("%s %s %3.2f\n", dt.nama, dt.pesanan, dt.harga_total);
-    }
-    fclose(f_pembeli);
+    // while ((fread(&dt_pelanggan, sizeof(dt_pelanggan), JUM_BLOK, f_pelanggan)) == JUM_BLOK)
+	// {
+	// 	printf("  %-5d %-10s %-10s %-10d %-10li\n", dt_pelanggan.code, dt_pelanggan.name, dt_pelanggan.type, dt_pelanggan.total, dt_pelanggan.price);
+	// }
+
+    // while (!feof(f_pelanggan))
+    // {
+    //     fscanf(f_pelanggan, "%s %s %f", &dt.nama, &dt.pelanggan, &dt.harga_total);
+    //     printf("%s %s %3.2f\n", dt.nama, dt.pelanggan, dt.harga_total);
+    // }
+    // fclose(f_pelanggan);
 }
 
 void buat_file_transaksi(){
     // buat file transaksi
-	Pembeli dt;
-    FILE *f_pembeli;
-	if ( (f_pembeli=fopen("data_transaksi.dat","wb")) == NULL )
+	pelanggan dt;
+    FILE *f_pelanggan;
+	if ( (f_pelanggan=fopen("data_transaksi.dat","wb")) == NULL )
     {
         printf("File tidak dapat dibuat!\r\n");
         exit(1);
--- transaksi.h
+++ transaksi.h
@@ -1,13 +1,18 @@
 #ifndef transaksi_h
 #define transaksi_h
+#define JUM_BLOK 1
 
 void transaksi();
 void buat_file_transaksi();
 
 typedef struct
 {
-    char nama[3];
-    char pesanan[11];
+    int id_transaksi;
+    char nama[20];
+    char makanan[20];
+    char minuman[20];
+    float harga_normal;
+    float diskon;
     float harga_total;
-} Pembeli;
+} pelanggan;
 #endif
