4bd0e617852a9f1d627dbd631285a3be14e18921
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-01-01 15:33:15 +0700

    FIX : buat file pelangan buat fitur getname, view welcome

--- beranda.cpp
+++ beranda.cpp
@@ -1,4 +1,43 @@
 #include <iostream>
+#include "pelanggan.h"
+#include "app.h"
+
+void welcome(){
+
+    char pilih;
+
+    printf("!=============================================================|\n");
+    printf("|                         SELAMAT DATANG                      |\n");
+    printf("!=============================================================|\n");
+    printf("|                                                             |\n");
+    printf("|                      Y. Mulai Memesan                       |\n");
+    printf("|   T. Tentang                                  E. Keluar     |\n");
+    printf("!=============================================================|\n");
+    printf("|                 Copyright 2021 - RestoApp                   |\n");
+    printf("!=============================================================|\n");
+
+    printf("\n\t Masukkan pilihan anda : ");
+    scanf("%s", &pilih);
+    if (pilih == 'y' || pilih == 'Y')
+    {
+        system("cls");
+        getNama();
+    	app();
+    }else if (pilih == 't' || pilih == 'T'){
+        system("cls");
+        printf("\n\t Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam suscipit corrupti sequi incidunt eaque rerum dolore nam laboriosam eos, error placeat. Similique, sequi.\n");
+        
+    }else if (pilih == 'e' || pilih == 'E'){
+        system("cls");
+    }else{
+        system("cls");
+        printf("\n\t Inputan yang anda masukkan salah !!! ");
+        welcome(); // function rekursif
+    }
+
+
+}
+
 
 void beranda() {
     
