f2b4e8ca3364003176ac0cb1a34d92531842ef80
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-01-02 09:30:51 +0700

    FIX : pelanggan

--- pelanggan.cpp
+++ pelanggan.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 
+typedef struct{
+    char nama[20];
+} pelanggan;
+
 void getNama() {
     char nama[20];
 
     // input nama
-    
-
     printf("\n\t Masukkan nama anda : ");
+    scanf("%s", &nama);
 
+    // save to file pelanggan.txt
+    pelanggan dt_pelanggan;
+    FILE *f_pelanggan;
+    f_pelanggan = fopen("pelanggan.dat", "ab+");
+    if(!f_pelanggan) {
+        printf("\n\t Gagal membuat file pelanggan.dat");
+        exit(1);
+    }
+    // fprintf(file, "%s", nama);
 
-    fscanf(stdin, "%s", &nama);
+    fwrite(&dt_pelanggan, sizeof(dt_pelanggan), 1, f_pelanggan);
+
+    fclose(f_pelanggan);
 
 }
